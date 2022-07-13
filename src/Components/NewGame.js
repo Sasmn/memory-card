39bc718932e0257112c24a817ae553c6214eddf8
sasmn <0.3sasi@gmail.com>
2022-07-13 19:36:16 +0200

    style button and transition optimization

--- src/Components/NewGame.js
+++ src/Components/NewGame.js
@@ -8,9 +8,13 @@ const NewGame = () => {
 
   return (
     <div ref={page} className={context.end === true ? "page in" : "page out"}>
-      <h1>
-        Congratulations, you won! Your score is 12, you found every image!
-      </h1>
+      {context.currentScore === 12 ? (
+        <h1>
+          Congratulations, you won! Your score is 12, you found every image!
+        </h1>
+      ) : (
+        <h1>Try again!</h1>
+      )}
       <button onClick={context.toggleEnd}>New game</button>
     </div>
   );
