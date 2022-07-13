9dda5aa18db268cf63a8ab21c9398b30e46455bb
sasmn <0.3sasi@gmail.com>
2022-07-13 10:52:25 +0200

    add bakcground color to card

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "../styles/Card.scss";
+import { FastAverageColor } from "fast-average-color";
+const fac = new FastAverageColor();
 
 const Card = (props) => {
+  const img = useRef();
+  const card = useRef();
+  useEffect(() => {
+    fac
+      .getColorAsync(img.current, {
+        ignoredColor: [255, 255, 255, 255, 20],
+      })
+      .then((color) => {
+        card.current.style.backgroundColor = color.rgba;
+      });
+  });
+
   return (
-    <div onClick={() => props.handleClick(props.id)} className="card">
-      <img src={props.src} alt={props.name} />
+    <div
+      onClick={() => props.handleClick(props.id)}
+      className="card"
+      ref={card}
+    >
+      <img ref={img} src={props.src} alt={props.name} />
       <h4>{props.name}</h4>
     </div>
   );
